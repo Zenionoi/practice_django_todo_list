2f0fd0b45cbfa6d87c040980fe38f3c24d3cb9bc
Zenon <zenion@ukr.net>
2023-04-03 11:12:45 +0300

    Added changes for admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+from django.contrib.admin import site
+from django.contrib.auth.models import Group, User
 
-# Register your models here.
+from .models import Tag, Task
+
+site.unregister(Group)
+site.unregister(User)
+site.register(Tag)
+site.register(Task)
